f04ef27c3f20fc10ac5e11e9e8a7b553f983e1da
Maria Komarova <komarova@phystech.edu>
2021-05-21 12:33:53 +0300

    Fill PageNotFound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Main from '../Main/Main'
-import Movies from '../Movies/Movies'
-import SavedMovies from '../SavedMovies/SavedMovies'
-import Profile from '../Profile/Profile'
-import Login from '../Login/Login'
-import Register from '../Register/Register'
-import PageNotFound from '../PageNotFound/PageNotFound'
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 function App() {
   return (
@@ -33,22 +33,6 @@ function App() {
         <PageNotFound />
       </Route>
     </Switch>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,6 +1,13 @@
+import { Link } from 'react-router-dom';
+import './PageNotFound.css'
+
 function PageNotFound() {
     return (
-        <div>404</div>
+        <section className='no-page'>
+            <p className='no-page__code'>404</p>
+            <p className='no-page__message'>Запрашиваемая страница не найдена</p>
+            <Link to='/' className='no-page__link'>Назад</Link>
+        </section>
     )
 }
 
