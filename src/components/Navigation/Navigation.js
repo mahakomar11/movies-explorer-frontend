333bd6f7aa7a7af99182583ba1f0d834a5d01c44
Maria Komarova <komarova@phystech.edu>
2021-05-22 13:47:44 +0300

    Fill Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+import './Navigation.css';
+
 function Navigation() {
-    return (
-        <div>Це навигация</div>
-    )
+  return (
+    <nav>
+      <Link to='/signup' className='nav__link'>
+        Регистрация
+      </Link>
+      <button className='nav__button'>Войти</button>
+    </nav>
+  );
 }
 
 export default Navigation;
