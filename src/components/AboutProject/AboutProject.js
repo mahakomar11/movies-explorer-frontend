dd19041c272423e1f7dcdddba3018fa06b985675
Maria Komarova <komarova@phystech.edu>
2021-05-23 19:51:46 +0300

    Fill Techs

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,7 +3,7 @@ import './AboutProject.css';
 function AboutProject() {
   return (
     <section className='about-project'>
-      <h2 className='about-project__title'>О проекте</h2>
+      <h2 className='subtitle'>О проекте</h2>
       <div className='about-project__info'>
         <div className='about-project__block'>
           <h3 className='about-project__highlight'>
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,7 +1,25 @@
 import './Techs.css';
 
 function Techs() {
-  return <section>Це Techs</section>;
+  return (
+    <section className='techs'>
+      <h2 className='subtitle'>О проекте</h2>
+      <h3 className='techs__subtitle'>7 технологий</h3>
+      <p className='techs__text'>
+        На курсе веб-разработки мы освоили технологии, которые применили в
+        дипломном проекте.
+      </p>
+      <div className='techs__stack'>
+        <p className='techs__tech'>HTML</p>
+        <p className='techs__tech'>CSS</p>
+        <p className='techs__tech'>JS</p>
+        <p className='techs__tech'>React</p>
+        <p className='techs__tech'>Git</p>
+        <p className='techs__tech'>Express.js</p>
+        <p className='techs__tech'>mongoDB</p>
+      </div>
+    </section>
+  );
 }
 
 export default Techs;
