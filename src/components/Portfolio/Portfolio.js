bce3c7e8236e4b2b241c5c651ed216cdc8e223d0
Maria Komarova <komarova@phystech.edu>
2021-05-25 15:01:48 +0300

    Fill Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,28 @@
 import './Portfolio.css';
 
 function Portfolio() {
-  return <section>Це Portfolio</section>;
+  return (
+    <section className='portfolio'>
+      <h2 className='portfolio__title'>Портфолио</h2>
+      <ul className='portfolio__list'>
+        <li className='portfolio__item'>
+          <a className='portfolio__link' href='#'>
+            Статичный сайт<span>↗</span>
+          </a>
+        </li>
+        <li className='portfolio__item'>
+          <a className='portfolio__link' href='#'>
+            Адаптивный сайт<span>↗</span>
+          </a>
+        </li>
+        <li className='portfolio__item'>
+          <a className='portfolio__link' href='#'>
+            Одностраничное приложение<span>↗</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
 export default Portfolio;
