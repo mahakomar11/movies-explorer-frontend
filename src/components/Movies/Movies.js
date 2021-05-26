864f704cce3cf75f9fe9636f19991cdb583a246b
Maria Komarova <komarova@phystech.edu>
2021-05-26 10:51:41 +0300

    Fill SearchForm

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,10 +1,12 @@
 import Header from '../Header/Header';
+import SearchForm from '../SearchForm/SearchForm';
 import Footer from '../Footer/Footer';
 
 function Movies() {
     return (
         <>
             <Header isLogined={true}/>
+            <SearchForm />
             <Footer />
         </>
     )
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,7 +2,11 @@ import './SearchForm.css';
 
 function SearchForm() {
   return (
-    <section>
+    <section className='search-form'>
+      <div className='search-form__container'>
+        <input placeholder='Фильм' className='search-form__input'></input>
+        <button className='search-form__button'></button>
+      </div>
     </section>
   );
 }
