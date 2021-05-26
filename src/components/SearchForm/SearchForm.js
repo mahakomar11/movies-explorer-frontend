49c10ac7256e4902c845a7cfc4385ae349cdeb30
Maria Komarova <komarova@phystech.edu>
2021-05-26 13:13:52 +0300

    Add slider checkbox

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,9 +3,16 @@ import './SearchForm.css';
 function SearchForm() {
   return (
     <section className='search-form'>
-      <div className='search-form__container'>
-        <input placeholder='Фильм' className='search-form__input'></input>
+      <div className='search-form__search-container'>
+        <input placeholder='Фильм' className='search-form__input' />
         <button className='search-form__button'></button>
+      </div>
+      <div className='search-form__checkbox-container'>
+        <label className='search-form__switch'>
+          <input className='search-form__checkbox' type='checkbox' />
+          <span className='search-form__slider'></span>
+        </label>
+        <p className='search-form__checkbox-message'>Короткометражки</p>
       </div>
     </section>
   );
