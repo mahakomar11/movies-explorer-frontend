ad0495bee080aa0761607bb87ef0a4b3f5c64868
Maria Komarova <komarova@phystech.edu>
2021-05-29 15:58:15 +0300

    Fix PageNotFound

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -4,8 +4,8 @@ import './PageNotFound.css';
 function PageNotFound() {
   return (
     <section className='no-page'>
-      <p className='no-page__code'>404</p>
-      <p className='no-page__message'>Запрашиваемая страница не найдена</p>
+        <p className='no-page__code'>404</p>
+        <p className='no-page__message'>Страница не найдена</p>
       <Link to='/' className='no-page__link'>
         Назад
       </Link>
