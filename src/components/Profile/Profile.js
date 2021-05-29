5e4db34e688cc24a839145b4c62aee68854629f7
Maria Komarova <komarova@phystech.edu>
2021-05-29 14:39:57 +0300

    Edit semantic of Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,23 +5,31 @@ function Profile() {
   return (
     <section className='profile'>
       <Header isLogined={true} />
-      <div className='profile__container'>
-        <h1 className='profile__greetings'>Привет, Виталя!</h1>
-        <p className='profile__block profile__block_info_name'>
-          <p className='profile__block-label'>Имя</p>
-          <input className='profile__block-value' value='Виталя' disabled/>
-        </p>
-        <p className='profile__block profile__block_info_email'>
-          <p className='profile__block-label'>E-mail</p>
-          <input className='profile__block-value' value='pochta@yandex.ru' disabled/>
-        </p>
-        <button className='profile__button profile__button_action_edit'>
-          Редактировать
-        </button>
-        <button className='profile__button profile__button_action_logout'>
-          Выйти из аккаунта
-        </button>
-      </div>
+      <h1 className='profile__greetings'>Привет, Виталя!</h1>
+      <form className='profile__form'>
+        <fieldset className='profile__fieldset'>
+          <label className='profile__label'>
+            Имя
+            <input className='profile__input' value='Виталя' disabled/>
+          </label>
+          <label className='profile__label'>
+            E-mail
+            <input
+              className='profile__input'
+              value='pochta@yandex.ru'
+              disabled
+            />
+          </label>
+        </fieldset>
+        <fieldset className='profile__fieldset'>
+          <button className='profile__button profile__button_action_edit'>
+            Редактировать
+          </button>
+          <button className='profile__button profile__button_action_logout'>
+            Выйти из аккаунта
+          </button>
+        </fieldset>
+      </form>
     </section>
   );
 }
