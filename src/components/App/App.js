77111cbc816cdc1eb059ef070b8745f13117366f
Maria Komarova <komarova@phystech.edu>
2021-05-30 11:33:28 +0300

    Make adaptive Header (not logined)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,9 @@ import Login from '../Login/Login';
 import Register from '../Register/Register';
 import PageNotFound from '../PageNotFound/PageNotFound';
 
+// TODO: адаптировать
+// TODO: сжать изображения
+
 function App() {
   return (
     <Switch>
