a75a035d3befc571e79124a7bd2c81075d82396c
Maria Komarova <komarova@phystech.edu>
2021-06-01 15:12:54 +0300

    Add movies to saved movies

--- src/temporary/moviesCards.js
+++ src/temporary/moviesCards.js
@@ -1,25 +1,28 @@
 import cover from './cover.svg';
 
-let moviesCards = Array(12).fill({
+let moviesCards = Array(11).fill({
   name: 'В погоне за Бэнкси',
   duration: '27минут',
   isSaved: false,
   image: cover,
 });
 
-moviesCards.unshift({
-  name: 'В погоне за Бэнкси',
-  duration: '27минут',
-  isSaved: true,
-  image: cover,
-});
+// moviesCards.unshift({
+//   name: 'В погоне за Бэнкси',
+//   duration: '27минут',
+//   isSaved: true,
+//   image: cover,
+// });
 
-const savedMoviesCards = [
+const savedMoviesCards = Array(3).fill(
   {
     name: 'В погоне за Бэнкси',
     duration: '27минут',
+    isSaved: true,
     image: cover,
   },
-];
+);
+
+moviesCards = [...savedMoviesCards, ...moviesCards];
 
 export { moviesCards, savedMoviesCards };
