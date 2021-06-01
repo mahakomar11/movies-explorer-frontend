347cda5b7dcfc3ec3f613199abb4e8d3a618ec4f
Maria Komarova <komarova@phystech.edu>
2021-06-01 15:00:29 +0300

    Add focus to search-form input and hover to its button

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,12 +4,22 @@ function SearchForm() {
   return (
     <form className='search-form'>
       <fieldset className='search-form__fieldset sign-form__fieldset_for_search'>
-        <input placeholder='Фильм' className='search-form__input' type='text'/>
+        <input
+          placeholder='Фильм'
+          className='search-form__input'
+          name='movie'
+          type='text'
+          required
+        />
         <button className='search-form__button' type='submit'></button>
       </fieldset>
       <fieldset className='search-form__fieldset sign-form__fieldset_for_checkbox'>
         <label className='search-form__switch'>
-          <input className='search-form__checkbox' type='checkbox' />
+          <input
+            className='search-form__checkbox'
+            type='checkbox'
+            name='is-short'
+          />
           <span className='search-form__slider'></span>
         </label>
         <p className='search-form__checkbox-message'>Короткометражки</p>
