aba6f025f1c23cd688a4f2eb570adee2321b1b30
Maria Komarova <komarova@phystech.edu>
2021-06-02 12:43:21 +0300

    Handle login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
-import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
@@ -7,9 +8,36 @@ import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
 import PageNotFound from '../PageNotFound/PageNotFound';
-
+import mainApi from '../../utils/MainApi';
 
 function App() {
+  const history = useHistory();
+  const [loggedIn, setLoggedIn] = React.useState(false);
+
+  function handleLogin(loginData) {
+    mainApi
+      .loginUser(loginData)
+      .then((data) => {
+        setLoggedIn(true);
+        localStorage.setItem('jwt', data.token);
+        history.push('/movies');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  function handleLogout() {
+    localStorage.removeItem('jwt');
+    setLoggedIn(false);
+    mainApi.logoutUser();
+  }
+  // React.useEffect(() => {
+  //   mainApi
+  //     .loginUser({ email: 'isis@email.com', password: '88888888' })
+  //     .then((data) => console.log(data));
+  // }, []);
+
   return (
     <Switch>
       <Route exact path='/'>
@@ -25,7 +53,7 @@ function App() {
         <Profile />
       </Route>
       <Route exact path='/signin'>
-        <Login />
+        <Login onLogin={handleLogin}/>
       </Route>
       <Route exact path='/signup'>
         <Register />
--- src/components/SignForm/SignForm.js
+++ src/components/SignForm/SignForm.js
@@ -1,14 +1,43 @@
+import React from 'react';
 import './SignForm.css';
 import { Link } from 'react-router-dom';
 
 function SignForm(props) {
+  const { place, onSubmit } = props;
+
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  React.useEffect(() => {
+    setName('');
+    setEmail('');
+    setPassword('');
+  }, []);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (place === 'signup') onSubmit({ name, email, password });
+    else onSubmit({ email, password });
+  }
+
   const message =
-    props.place === 'signup'
-      ? 'Уже зарегистрированы? '
-      : 'Ещё не зарегистрированы? ';
+    place === 'signup' ? 'Уже зарегистрированы? ' : 'Ещё не зарегистрированы? ';
 
   const link =
-    props.place === 'signup' ? (
+    place === 'signup' ? (
       <Link to='/signin' className='sign-form__link'>
         Войти
       </Link>
@@ -19,11 +48,11 @@ function SignForm(props) {
     );
 
   return (
-    <form className='sign-form'>
+    <form className='sign-form' onSubmit={handleSubmit}>
       <fieldset className='sign-form__fieldset'>
-        {props.place === 'signup' && (
+        {place === 'signup' && (
           <>
-            <label className='sign-form__label' for='name'>
+            <label className='sign-form__label' htmlFor='name'>
               Имя
             </label>
             <input
@@ -31,14 +60,16 @@ function SignForm(props) {
               type='text'
               name='name'
               id='name'
+              value={name}
               minLength={2}
               maxLength={40}
               required
+              onChange={handleNameChange}
             />
             <span className='sign-form__error-message' id='name-error'></span>
           </>
         )}
-        <label className='sign-form__label' for='email'>
+        <label className='sign-form__label' htmlFor='email'>
           E-mail
         </label>
         <input
@@ -46,11 +77,13 @@ function SignForm(props) {
           type='email'
           name='email'
           id='email'
+          value={email}
           minLength={2}
           required
+          onChange={handleEmailChange}
         />
         <span className='sign-form__error-message' id='email-error'></span>
-        <label className='sign-form__label' for='password'>
+        <label className='sign-form__label' htmlFor='password'>
           Пароль
         </label>
         <input
@@ -58,14 +91,16 @@ function SignForm(props) {
           type='password'
           name='password'
           id='password'
+          value={password}
           minLength={8}
           required
+          onChange={handlePasswordChange}
         />
         <span className='sign-form__error-message' id='password-error'></span>
       </fieldset>
       <fieldset className='sign-form__fieldset'>
         <button className='sign-form__button' type='submit'>
-          {props.place === 'signup' ? 'Зарегистрироваться' : 'Войти'}
+          {place === 'signup' ? 'Зарегистрироваться' : 'Войти'}
         </button>
         <p className='sign-form__message'>
           {message}
